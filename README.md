f297a36a959b3cbe6c5296baa4b256517e2e5d4b
burashka <burashka-lcme@yandex.ru>
2017-10-25 10:37:47 +0300

    Move to package

---
+++ README.md
@@ -0,0 +1,36 @@
+Find Developers by Projects
+===========================
+
+Description
+-----------
+
+Having the need/want to look up good developers on GitHub. We require candidates to know certain frameworks or libraries.
+It is logical to look for developers among their contributors. This module allows you to find all the contributors of
+these projects from the specified region.
+
+Parameters
+----------
+
+| Name 			| Type 			| Description 			|
+| ------------- | ------------- | ---- 					|
+| authenticate 	| object 		| GitHub credentials 	|
+| repos 		| Array, String | Repositories for find |
+| locations 	| Array, String | Locations for find 	|
+
+Usage
+-----
+
+```javascript
+const find = require("find-devs-by-project");
+
+find({
+	authenticate: {
+		type: "token",
+		token: "<your token>"
+	},
+	repos: ["dojo/dojo", "dojo/dijit", "dojo/dojox", "dojo/util"],
+	locations: ["Russia", "Moscow"]
+}).then(results => {
+	results.forEach(repo => repo.forEach(user => console.log(user.login, user.html_url, "\n")));
+});
+```
--- app.js
+++ app.js
@@ -1,60 +1,46 @@
 const GitHubApi = require("github");
 
 const github = new GitHubApi({
-	version: "3.0.0",
-	// debug: true
+	version: "3.0.0"
 });
 
 async function find({ authenticate, repos, locations }) {
 	github.authenticate(authenticate);
 
+	repos = Array.isArray(repos) ? repos : [repos];
 	locations = Array.isArray(locations) ? locations : [locations];
 	locations = new RegExp(locations.join("|"), "gi");
 
-	function checkUser(username){
+	function checkUser(user){
+		const username = user.login || user.owner.login;
+
 		return github.users.getForUser({ username }).then(
 			({data}) => {
 				if (data.location && locations.test(data.location)){
-					console.log(data.login, data.html_url);
 					return data;
 				}
 
 				return false;
 			},
-			err => console.log(err)
+			err => console.log(`Check user failed: ${err}`)
 		);
 	}
 
 	const promiseUsers = repos.map(repository => {
 		const [owner, repo] = repository.split("/");
 
-/*		Promise.all([
+		return Promise.all([
 			github.repos.getContributors({ repo, owner }),
 			github.repos.getForks({ repo, owner })
-		]).then(
+		])
+		.then(
 			([contributors, forks]) => {
-				contributors.data.concat(forks.data).forEach(item => checkUser(item.login || item.owner.login));
-			},
-			err => console.log(err)
-		);	*/
-
-		const promiseUsersFromContribs = github.repos.getContributors({ repo, owner })
-			.then(({ data }) => data.map(({login}) => checkUser(login)))
-			.then(
-				promiseUsers => Promise.all(promiseUsers),
-				err => console.log(err)
-			);
-
-		const promiseUsersFromForks = github.repos.getForks({repo, owner})
-			.then(({data}) => data.map(({owner}) => checkUser(owner.login)))
-			.then(
-				promiseUsers => Promise.all(promiseUsers),
-				err => console.log(err)
-			);
-
-		return Promise.all([ promiseUsersFromContribs, promiseUsersFromForks ]).then(
-			([ usersFromContribs, usersFromForks ]) => usersFromContribs.concat(usersFromForks).filter(user => !!user),
-			err => console.log(err)
+				return Promise.all(contributors.data.concat(forks.data).map(user => checkUser(user)));
+			}
+		)
+		.then(
+			users => users.filter(user => !!user),
+			err => console.log(`Get contributors and forks failed: ${err}`)
 		);
 	});
 
